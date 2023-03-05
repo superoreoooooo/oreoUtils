edda8519af873087cd23bdd9dfba674c53c06fa1
oreo <oooowwww365@gmail.com>
2023-03-05 13:06:00 +0900

    Initial commit

---
+++ src/main/java/win/oreo/oreoutils/OreoUtils.java
@@ -0,0 +1,17 @@
+package win.oreo.oreoutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OreoUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
